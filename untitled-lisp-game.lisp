e86999316e978430c72c0cc9cb971a19ce63b7c4
Emil Segerbäck <emilsegerback@gmail.com>
2018-02-24 16:40:54 +0100

    Make code more recompile-friendly, try uniforms and add autoresizing of viewport

--- untitled-lisp-game.lisp
+++ untitled-lisp-game.lisp
@@ -2,45 +2,47 @@
 
 (in-package #:untitled-lisp-game)
 
-(defparameter *array* nil)
-(defparameter *stream* nil)
-(defparameter *running* nil)
+(defvar *array* nil)
+(defvar *stream* nil)
+(defvar *running* nil)
 
-;; Lisp data for triangle vertices
 (defparameter *triangle-data*
    (list (list (v!  0.5 -0.36 0) (v! 0 1 0 1))
          (list (v!    0   0.5 0) (v! 1 0 0 1))
          (list (v! -0.5 -0.36 0) (v! 0 0 1 1))))
 
-;; A struct that works on gpu and cpu
 (defstruct-g pos-col
   (position :vec3 :accessor pos)
   (color :vec4 :accessor col))
 
-;; A GPU vertex shader using a Lisp syntax (see Varjo)
-(defun-g vert ((vert pos-col))
-  (values (v! (pos vert) 1.0)
+(defun-g vert ((vert pos-col) &uniform (mat :mat4))
+  (values (* mat (v! (pos vert) 1.0))
           (col vert)))
 
-;; A GPU fragment shader
 (defun-g frag ((color :vec4))
   color)
 
-;; Composing those gpu functions into a pipeline
 (defpipeline-g prog-1 ()
   (vert pos-col)
   (frag :vec4))
 
+(defun current-window-size ()
+  (destructuring-bind (w h)
+      (cepl.host:window-size (current-surface (cepl-context)))
+    (v! w h)))
+
 ;; Here is what we do each frame:
-(defun step-demo ()
+(defun step-game ()
   ;; Advance the host environment frame
   (step-host)
-   ;; Keep the REPL responsive while running
+  ;; Make the viewport fill the whole screen
+  (setf (viewport-resolution (current-viewport)) (current-window-size))
+  ;; Keep the REPL responsive while running
   (update-repl-link)
   ;; Clear the drawing buffer
   (clear)
   ;; Render data from GPU datastream
-  (map-g #'prog-1 *stream*)
+  (map-g #'prog-1 *stream* :mat (m4:translation (v! 0.0 0.5 0.0)))
   ;; Display newly rendered buffer
   (swap))
 
@@ -49,9 +51,9 @@
   (setf *running* t
         *array* (make-gpu-array *triangle-data* :element-type 'pos-col)
         *stream* (make-buffer-stream *array*))
-  (loop :while (and  *running*
+  (loop :while (and *running*
              (not (shutting-down-p))) :do
-     (continuable (step-demo))))
+     (continuable (step-game))))
 
 (defun stop-loop ()
   (setf *running* nil))
